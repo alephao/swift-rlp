51cc7d459d7aacc4d86bd499df27bba2ff5ba7ed
Aleph Retamal <retamal.ph@gmail.com>
2018-01-31 22:00:24 +1100

    init

---
+++ RLPSwift/RLPSwift.h
@@ -0,0 +1,19 @@
+//
+//  RLPSwift.h
+//  RLPSwift
+//
+//  Created by Aleph Retamal on 31/1/18.
+//  Copyright © 2018 Lalacode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RLPSwift.
+FOUNDATION_EXPORT double RLPSwiftVersionNumber;
+
+//! Project version string for RLPSwift.
+FOUNDATION_EXPORT const unsigned char RLPSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RLPSwift/PublicHeader.h>
+
+
