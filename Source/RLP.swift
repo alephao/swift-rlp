e2b8f792e4bd95957328e97ccee68045c26ef96f
Aleph Retamal <retamal.ph@gmail.com>
2018-02-02 00:54:26 +1100

    WIP - encode using data

--- Source/RLP.swift
+++ Source/RLP.swift
@@ -6,6 +6,7 @@ public enum RLP {
     public enum Error: Swift.Error {
         case unicodeOutOfBoundaries
         case stringToData
+        case dataToString
         case invalidType(Any)
     }
 
@@ -13,48 +14,58 @@ public enum RLP {
         return UInt8(ceil(log10(Double(n))/log10(Double(UInt8.max))))
     }
     
-    static func encodeLength(_ length: UInt32, offset: UInt8) throws -> String {
+    static func encodeLength(_ length: UInt32, offset: UInt8) -> Data {
         if length < 56 {
             let lengthByte = offset + UInt8(length)
-            return String(Unicode.Scalar(lengthByte))
+            return Data(bytes: [lengthByte])
         } else {
             let firstByte = offset + 55 + binaryLength(of: length)
-            guard let lengthUnicode = Unicode.Scalar(length) else {
-                throw Error.unicodeOutOfBoundaries
-            }
-            return "\(Unicode.Scalar(firstByte))\(lengthUnicode)"
+            var bytes = [firstByte]
+            bytes.append(contentsOf: length.byteArrayLittleEndian)
+            return Data(bytes: bytes)
+        }
+    }
+    
+    public static func encode(_ bytes: Data) -> Data {
+        if bytes.count == 1,
+            0x00...0x7f ~= bytes[0] {
+            return bytes
+        } else {
+            var result = encodeLength(UInt32(bytes.count), offset: 0x80)
+            result.append(contentsOf: bytes)
+            return result
         }
     }
     
     public static func encode(_ string: String) throws -> String {
-        guard let data = string.data(using: .utf8) else {
+        guard let data = string.data(using: .ascii) else {
             throw Error.stringToData
         }
+
+        let bytes = encode(data)
         
-        if data.count == 1,
-            0x00...0x7f ~= data[0] {
-            return string
-        } else {
-            let encodedLength = try encodeLength(UInt32(data.count), offset: 0x80)
-            return encodedLength + string
-        }
-    }
-    
-    public static func encode(_ array: [Any]) throws -> String {
-        var output = ""
-        
-        for item in array {
-            if let string = item as? String {
-                output += try encode(string)
-            } else if let array = item as? [Any] {
-                output += try encode(array)
-            } else {
-                throw Error.invalidType(item)
-            }
+        guard let result = String(data: bytes, encoding: .ascii) else {
+            throw Error.dataToString
         }
         
-        let encodedLength = try encodeLength(UInt32(output.count), offset: 0xc0)
-        return encodedLength + output
+        return result
     }
+//
+//    public static func encode(_ array: [Any]) throws -> String {
+//        var output = ""
+//
+//        for item in array {
+//            if let string = item as? String {
+//                output += try encode(string)
+//            } else if let array = item as? [Any] {
+//                output += try encode(array)
+//            } else {
+//                throw Error.invalidType(item)
+//            }
+//        }
+//
+//        let encodedLength = try encodeLength(UInt32(output.count), offset: 0xc0)
+//        return encodedLength + output
+//    }
     
 }
--- Tests/RLPTests.swift
+++ Tests/RLPTests.swift
@@ -7,59 +7,46 @@ import RLPSwift
 
 class RLPTests: XCTestCase {
     
-    func testEncodeLength() {
-        XCTAssertEqual(try? RLP.encodeLength(0x00, offset: 0x80), "\u{80}")
-        XCTAssertEqual(try? RLP.encodeLength(0x0400, offset: 0x80), "\u{b9}\u{400}")
-        XCTAssertEqual(try? RLP.encodeLength(0x11170, offset: 0x80), "\u{ba}\u{11170}")
+    func testEncodeLengthSmallerThanOrEqualTo55() {
+        XCTAssertEqual(RLP.encodeLength(0x00, offset: 0x80), Data(bytes: [0x80]))
+        XCTAssertEqual(RLP.encodeLength(0x37, offset: 0x80), Data(bytes: [0xb7]))
     }
     
-    func testEncodeEmptyString() {
-        XCTAssertEqual(try? RLP.encode(""), "\u{80}")
+    func testEncodeLengthGreaterThan55() {
+        XCTAssertEqual(RLP.encodeLength(0x38, offset: 0x80), Data(bytes: [0xb8, 0x0, 0x0, 0x0, 0x38]))
+        XCTAssertEqual(RLP.encodeLength(0x0400, offset: 0x80), Data(bytes: [0xb9, 0x0, 0x0, 0x04, 0x0]))
+        XCTAssertEqual(RLP.encodeLength(0x11170, offset: 0x80), Data(bytes: [0xba, 0x0, 0x01, 0x11, 0x70]))
     }
     
-    func testEncodeStringWithSingleByte() {
+    func testEncodeEmptyData() {
+        let subject = Data()
+        XCTAssertEqual(RLP.encode(subject), Data(bytes: [0x80]))
+    }
+
+    func testEncodeDataWithSingleByte() {
         XCTAssertEqual(try? RLP.encode("0"), "0")
         XCTAssertEqual(try? RLP.encode("a"), "a")
         XCTAssertEqual(try? RLP.encode("{"), "{")
     }
     
+    func testEncodeEmptyString() {
+        XCTAssertEqual(try? RLP.encode(""), "\u{80}")
+    }
+
     func testEncodeStringSmallerThanOrEqualTo55Bytes() {
         XCTAssertEqual(try? RLP.encode("100"), "\u{83}100")
         XCTAssertEqual(try? RLP.encode("Lorem ipsum dolor sit amet"), "\u{9a}Lorem ipsum dolor sit amet")
-        
+
         let string55 = [String](repeating: "a", count: 55).reduce("", +)
         XCTAssertEqual(try? RLP.encode(string55), "\u{b7}\(string55)")
     }
-    
+
     func testEncodeStringGreaterThan55Bytes() {
         XCTAssertEqual(try? RLP.encode("Lorem ipsum dolor sit amet, consectetur adipisicing elit"), "\u{b8}\u{38}Lorem ipsum dolor sit amet, consectetur adipisicing elit")
-        
+
         let stringWith400a = [String](repeating: "a", count: 400).reduce("", +)
         XCTAssertEqual(try? RLP.encode(stringWith400a), "\u{b9}\u{190}\(stringWith400a)")
     }
-    
-    func testEncodeStringArray() {
-        XCTAssertEqual(try? RLP.encode(["cat", "dog"]), "\u{c8}\u{83}cat\u{83}dog")
-    }
-    
-    func testEncodeTheoreticalRepresentationOfTwo() {
-        let subject: [Any] = [ [], [[]], [ [], [[]] ] ]
-        XCTAssertEqual(try? RLP.encode(subject), "\u{c7}\u{c0}\u{c1}\u{c0}\u{c3}\u{c0}\u{c1}\u{c0}")
-    }
-    
-    func testEncodeTransactionObjectAsStringArray() {
-        let subject = [
-            ["from", "0xb60e8dd61c5d32be8058bb8eb970870f07233155"],
-            ["to", "0xd46e8dd67c5d32be8058bb8eb970870f07244567"],
-            ["gas", "0x76c0"],
-            ["gasPrice", "0x9184e72a000"],
-            ["value", "0x9184e72a"],
-            ["data", "0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"]
-        ]
-        
-        let expected = "\u{f8}\u{f3}\u{f0}\u{84}from\u{aa}0xb60e8dd61c5d32be8058bb8eb970870f07233155\u{ee}\u{82}to\u{aa}0xd46e8dd67c5d32be8058bb8eb970870f07244567\u{cb}\u{83}gas\u{86}0x76c0\u{d7}\u{88}gasPrice\u{8d}0x9184e72a000\u{d1}value\u{8a}0x9184e72a\u{f8}\u{5b}\u{84}data\u{b8}\u{54}0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675"
-        
-        XCTAssertEqual(try? RLP.encode(subject), expected)
-    }
+
 
 }
