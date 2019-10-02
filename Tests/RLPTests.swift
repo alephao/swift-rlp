06bfa6d8725789d7b678022b8ae2b04b543a1e6d
Jesse Anderson <jesse.anderson@brandtinfo.com>
2019-10-02 09:27:58 -0400

    updated swift version to 5, remove redundent public modifiers, updated test to remove deperecated keyword

--- Tests/RLPTests.swift
+++ Tests/RLPTests.swift
@@ -16,14 +16,14 @@ extension RLPTests {
     }
     
     func testEncodeLengthSmallerThanOrEqualTo55() {
-        XCTAssertEqual(RLP.encodeLength(0x00, offset: 0x80), Data(bytes: [0x80]))
-        XCTAssertEqual(RLP.encodeLength(0x37, offset: 0x80), Data(bytes: [0xb7]))
+        XCTAssertEqual(RLP.encodeLength(0x00, offset: 0x80), Data([0x80]))
+        XCTAssertEqual(RLP.encodeLength(0x37, offset: 0x80), Data([0xb7]))
     }
     
     func testEncodeLengthGreaterThan55() {
-        XCTAssertEqual(RLP.encodeLength(0x38, offset: 0x80), Data(bytes: [0xb8, 0x0, 0x0, 0x0, 0x38]))
-        XCTAssertEqual(RLP.encodeLength(0x0400, offset: 0x80), Data(bytes: [0xb9, 0x0, 0x0, 0x04, 0x0]))
-        XCTAssertEqual(RLP.encodeLength(0x11170, offset: 0x80), Data(bytes: [0xba, 0x0, 0x01, 0x11, 0x70]))
+        XCTAssertEqual(RLP.encodeLength(0x38, offset: 0x80), Data([0xb8, 0x0, 0x0, 0x0, 0x38]))
+        XCTAssertEqual(RLP.encodeLength(0x0400, offset: 0x80), Data([0xb9, 0x0, 0x0, 0x04, 0x0]))
+        XCTAssertEqual(RLP.encodeLength(0x11170, offset: 0x80), Data([0xba, 0x0, 0x01, 0x11, 0x70]))
     }
 }
 
@@ -32,30 +32,30 @@ extension RLPTests {
 extension RLPTests {
     func testEncodeEmptyData() {
         let subject = Data()
-        XCTAssertEqual(RLP.encode(subject), Data(bytes: [0x80]))
+        XCTAssertEqual(RLP.encode(subject), Data([0x80]))
     }
     
     func testEncodeDataWithSingleByte() {
-        XCTAssertEqual(RLP.encode(Data(bytes: [0x30])), Data(bytes: [0x30]))
-        XCTAssertEqual(RLP.encode(Data(bytes: [0x61])), Data(bytes: [0x61]))
-        XCTAssertEqual(RLP.encode(Data(bytes: [0x7b])), Data(bytes: [0x7b]))
+        XCTAssertEqual(RLP.encode(Data([0x30])), Data([0x30]))
+        XCTAssertEqual(RLP.encode(Data([0x61])), Data([0x61]))
+        XCTAssertEqual(RLP.encode(Data([0x7b])), Data([0x7b]))
     }
     
     func testEncodeDataSmallerThan55Bytes() {
-        XCTAssertEqual(RLP.encode(Data(bytes: [0x31, 0x30, 0x30])), Data(bytes: [0x83, 0x31, 0x30, 0x30]))
-        XCTAssertEqual(RLP.encode(Data(bytes: [0x4c, 0x6f, 0x72, 0x65, 0x6d, 0x20, 0x69, 0x70, 0x73, 0x75, 0x6d, 0x20, 0x64, 0x6f, 0x6c, 0x6f, 0x72, 0x20, 0x73, 0x69, 0x74, 0x20, 0x61, 0x6d, 0x65, 0x74])), Data(bytes: [0x9a, 0x4c, 0x6f, 0x72, 0x65, 0x6d, 0x20, 0x69, 0x70, 0x73, 0x75, 0x6d, 0x20, 0x64, 0x6f, 0x6c, 0x6f, 0x72, 0x20, 0x73, 0x69, 0x74, 0x20, 0x61, 0x6d, 0x65, 0x74]))
+        XCTAssertEqual(RLP.encode(Data([0x31, 0x30, 0x30])), Data([0x83, 0x31, 0x30, 0x30]))
+        XCTAssertEqual(RLP.encode(Data([0x4c, 0x6f, 0x72, 0x65, 0x6d, 0x20, 0x69, 0x70, 0x73, 0x75, 0x6d, 0x20, 0x64, 0x6f, 0x6c, 0x6f, 0x72, 0x20, 0x73, 0x69, 0x74, 0x20, 0x61, 0x6d, 0x65, 0x74])), Data([0x9a, 0x4c, 0x6f, 0x72, 0x65, 0x6d, 0x20, 0x69, 0x70, 0x73, 0x75, 0x6d, 0x20, 0x64, 0x6f, 0x6c, 0x6f, 0x72, 0x20, 0x73, 0x69, 0x74, 0x20, 0x61, 0x6d, 0x65, 0x74]))
     }
     
     func testEncodeDataEqualTo55Bytes() {
-        let subject = Data(bytes: [UInt8](repeating: 0x61, count: 55))
+        let subject = Data([UInt8](repeating: 0x61, count: 55))
         let expectedBytes = [0xb7] + subject
-        XCTAssertEqual(RLP.encode(subject), Data(bytes: expectedBytes))
+        XCTAssertEqual(RLP.encode(subject), Data(expectedBytes))
     }
     
     func testEncodeTheoreticalRepresentationOfTwo() {
         let subject: [Any] = [ [], [[]], [ [], [[]] ] ]
         
-        XCTAssertEqual(try RLP.encode(nestedArrayOfData: subject), Data(bytes: [0xc7, 0xc0, 0xc1, 0xc0, 0xc3, 0xc0, 0xc1, 0xc0]))
+        XCTAssertEqual(try RLP.encode(nestedArrayOfData: subject), Data([0xc7, 0xc0, 0xc1, 0xc0, 0xc3, 0xc0, 0xc1, 0xc0]))
     }
 }
 
@@ -63,29 +63,29 @@ extension RLPTests {
 extension RLPTests {
 
     func testEncodeEmptyString() {
-        XCTAssertEqual(try? RLP.encode(""), Data(bytes: [0x80]))
+        XCTAssertEqual(try? RLP.encode(""), Data([0x80]))
     }
     
     func testEncodeStringWithSingleByte() {
-        XCTAssertEqual(try? RLP.encode("0"), Data(bytes: [0x30]))
-        XCTAssertEqual(try? RLP.encode("a"), Data(bytes: [0x61]))
-        XCTAssertEqual(try? RLP.encode("{"), Data(bytes: [0x7b]))
+        XCTAssertEqual(try? RLP.encode("0"), Data([0x30]))
+        XCTAssertEqual(try? RLP.encode("a"), Data([0x61]))
+        XCTAssertEqual(try? RLP.encode("{"), Data([0x7b]))
     }
     
     func testEncodeStringSmallerThan55Bytes() {
-        XCTAssertEqual(try? RLP.encode("100"), Data(bytes: [0x83, 0x31, 0x30, 0x30]))
-        XCTAssertEqual(try? RLP.encode("Lorem ipsum dolor sit amet"), Data(bytes: [0x9a, 0x4c, 0x6f, 0x72, 0x65, 0x6d, 0x20, 0x69, 0x70, 0x73, 0x75, 0x6d, 0x20, 0x64, 0x6f, 0x6c, 0x6f, 0x72, 0x20, 0x73, 0x69, 0x74, 0x20, 0x61, 0x6d, 0x65, 0x74]))
+        XCTAssertEqual(try? RLP.encode("100"), Data([0x83, 0x31, 0x30, 0x30]))
+        XCTAssertEqual(try? RLP.encode("Lorem ipsum dolor sit amet"), Data([0x9a, 0x4c, 0x6f, 0x72, 0x65, 0x6d, 0x20, 0x69, 0x70, 0x73, 0x75, 0x6d, 0x20, 0x64, 0x6f, 0x6c, 0x6f, 0x72, 0x20, 0x73, 0x69, 0x74, 0x20, 0x61, 0x6d, 0x65, 0x74]))
     }
     
     func testEncodeStringEqualTo55Bytes() {
         let subject = [String](repeating: "a", count: 55).reduce("", +)
         let expectedBytes: [UInt8] = [0xb7] + [UInt8](repeating: 0x61, count: 55)
-        XCTAssertEqual(try? RLP.encode(subject), Data(bytes: expectedBytes))
+        XCTAssertEqual(try? RLP.encode(subject), Data(expectedBytes))
     }
 
     func testEncodeStringGreaterThan55Bytes() {
         let stringWith400a = [String](repeating: "a", count: 400).reduce("", +)
         let expectedBytes: [UInt8] = [0xb9, 0x00, 0x00, 0x01, 0x90] + [UInt8](repeating: 0x61, count: 400)
-        XCTAssertEqual(try? RLP.encode(stringWith400a), Data(bytes: expectedBytes))
+        XCTAssertEqual(try? RLP.encode(stringWith400a), Data(expectedBytes))
     }
 }
