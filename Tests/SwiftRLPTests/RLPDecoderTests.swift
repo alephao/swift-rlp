a6c18f6b11d690397d08c0adbc16a008cc333920
alephao <7674479+alephao@users.noreply.github.com>
2025-05-19 13:04:51 -0300

    test: update RLPDecoderTests to use swift-testing

--- Tests/SwiftRLPTests/RLPDecoderTests.swift
+++ Tests/SwiftRLPTests/RLPDecoderTests.swift
@@ -1,42 +1,40 @@
-import XCTest
+import Foundation
+import Testing
 
 @testable import SwiftRLP
 
-class DecodingTests: XCTestCase {}
-
-extension DecodingTests {
-
-    func assertDecoding(_ value: RLPValue) throws {
+struct RLPDecoderTests {
+    func assertRoundtrip(of value: RLPValue) throws {
         let encoder = RLPEncoder()
         let decoder = RLPDecoder()
 
         let encoded = try encoder.encode(value)
         let decoded = try decoder.decode(from: encoded)
 
-        XCTAssertEqual(decoded, value)
+        #expect(decoded == value)
     }
 
-    func testDecodeEmptyData() throws {
-        try assertDecoding(.string(""))
+    @Test func decodeEmptyString() throws {
+        try assertRoundtrip(of: .string(""))
     }
 
-    func testDecodeDataWithSingleByte() throws {
+    @Test func decodeDataWithSingleByte() throws {
         for str in ["0", "a", "{"] {
-            try assertDecoding(.string(str))
+            try assertRoundtrip(of: .string(str))
         }
     }
 
-    func testDecodeDataEqOrShorterThan55Bytes() throws {
+    @Test func decodeDataEqOrShorterThan55Bytes() throws {
         let byte = "a".data(using: .utf8)!.first!
 
         for l in 2...55 {
             let data = Data([UInt8](repeating: byte, count: l))
             let str = String(data: data, encoding: .utf8)!
-            try assertDecoding(.string(str))
+            try assertRoundtrip(of: .string(str))
         }
     }
 
-    func testEncodeStringLongerThan55Bytes() throws {
+    @Test func encodeStringLongerThan55Bytes() throws {
         let byte = "a".data(using: .utf8)!.first!
 
         for length in [
@@ -47,7 +45,7 @@ extension DecodingTests {
         ] {
             let data = Data([UInt8](repeating: byte, count: length))
             let str = String(data: data, encoding: .utf8)!
-            try assertDecoding(.string(str))
+            try assertRoundtrip(of: .string(str))
         }
     }
 }
